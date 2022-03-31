3b7c3666f41a8ebd0569d0e93f75f670166a2a4f
Chirag Bhalotia <me@chirag.codes>
2022-04-01 01:54:07 +0530

    changed width of account profile page

--- components/AccountPage/index.jsx
+++ components/AccountPage/index.jsx
@@ -1,6 +1,6 @@
 export default function AccountPage() {
     return (
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
             <form className="space-y-8 divide-y divide-gray-200">
                 <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
 
